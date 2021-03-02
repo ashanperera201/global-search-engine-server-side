75241e44391ade1d5f0ce6000a85f19088293292
Ashan Perera <ashanperera.201@gmail.com>
2021-03-02 09:53:10 +0530

    web scraping and request crawling simiulation

--- book.py
+++ book.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from business.book_manager import Book
+import requests
+import json
 
 
 app = Flask(__name__)
@@ -11,23 +13,25 @@ bookRef = Book()
 # Sample GET
 
 
-@app.route('/api/books')
+@app.route("/api/books")
 def get_books():
     books = bookRef.get_all_books()
     return jsonify(books)
 
+
 # Sample GET by id
 
 
-@app.route('/api/book/<string:name>')
+@app.route("/api/book/<string:name>")
 def get_book(name):
     book = bookRef.get_book(name)
     return jsonify(book)
 
+
 # Sample POST
 
 
-@app.route('/api/books', methods=['POST'])
+@app.route("/api/books", methods=["POST"])
 def add_book():
     book = request.get_json()
     savedVal = bookRef.add_book(book)
---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify
+from business.web_scrapy_manager import WebScraper
+from scrapy.crawler import CrawlerRunner
+from scrapy.signalmanager import dispatcher
+import crochet
+from scrapy import signals
+import requests
+import json
+
+crochet.setup()
+
+app = Flask(__name__)
+
+
+output_data = []
+
+
+crawl_runner = CrawlerRunner()
+
+
+@app.route("/scrape")
+def scrape():
+    scrape_with_crochet()
+    return jsonify(output_data)
+    
+
+@app.route('/api/requests/<string:term>')
+def search_term(term):
+    url = 'https://ikman.lk/data/serp'
+    params = {
+            "top_ads": 1,
+            "spotlights": 5,
+            "sort": "relevance",
+            "buy_now": 0,
+            "urgent": 0,
+            "categorySlug": "van",
+            "locationSlug": "colombo",
+            "category": 391,
+            "query": term,
+            "page": 1             
+        }
+    result = requests.get(url, params)
+    return result.json()
+
+
+@crochet.wait_for(timeout=10.0)
+def scrape_with_crochet():
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)
+    eventual = crawl_runner.crawl(WebScraper)
+    return eventual
+
+
+def _crawler_result(item, response, spider):
+    output_data.append(dict(item))
+
+
+app.run(port=5000)
